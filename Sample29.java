b6875c9d1b3c2bbb91632f09d37e7cada6fe640b
Makoto Tanabe <xekid78@outlook.com>
2018-01-23 20:54:24 +0900

    static変数

---
+++ Sample29.java
@@ -0,0 +1,32 @@
+/**
+ *
+ * @author xekid78
+ *
+ */
+public class Sample29 {
+
+	public static void main(String[] args) {
+		Item2 apple = new Item2(120, 15);
+		System.out.println("リンゴの合計金額は" + apple.gokei() + "円です。");
+		Item2 orenge = new Item2(85, 32);
+		System.out.println("オレンジの合計金額は" + orenge.gokei() + "円です。");
+
+	}
+
+}
+
+class Item2 {
+	private int tanka;
+	private int kosu;
+	private static double tax = 1.08;
+
+	public Item2(int tankax, int kosuy) {
+		tanka = tankax;
+		kosu = kosuy;
+	}
+
+	public int gokei() {
+		return (int)(tanka * kosu * tax);
+	}
+
+}
